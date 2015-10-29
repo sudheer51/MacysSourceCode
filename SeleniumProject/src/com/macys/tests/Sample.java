182bc51620e2fc6de4f5f250ea6c46301629032e
sudheer694@gmail.com <sudheer694@gmail.com>
2015-10-29 21:44:53 +0530

    Adding this file to head branch

---
+++ SeleniumProject/src/com/macys/tests/Sample.java
@@ -0,0 +1,20 @@
+package com.macys.tests;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	@Test
+	public void a()
+	{
+		System.out.println("in a");
+	}
+	@Test
+	public void b()
+	{
+		System.out.println("in b");
+	}
+	
+	
+
+}
